35a0f51c81eb8150b9dce51e119c70311dd5f515
Sophia Li <sophiali@Sophias-MacBook-Pro.local>
2023-12-20 00:36:46 -0500

    Began implementing FileDAO

---
+++ src/data_access/FileDataAccessObject.java
@@ -0,0 +1,23 @@
+package data_access;
+
+import entity.CommonBotMatchData;
+import use_case.load_bot.LoadBotDataAccessInterface;
+
+public class FileDataAccessObject implements LoadBotDataAccessInterface {
+    private final CommonBotMatchData botMatchData;
+
+    @Override
+    public Boolean loadBot(int botID) {
+        return null;
+    }
+
+    @Override
+    public Boolean existByBotID(int botID) {
+        return null;
+    }
+
+    @Override
+    public String getNameByBotID(int botID) {
+        return null;
+    }
+}
